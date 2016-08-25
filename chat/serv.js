50dbf47c8d7c1bfaacb642409d22cd5831193a69
Thibault FRANCOIS <thifranc@e1r1p8.42.fr>
2016-08-25 15:14:57 +0200

    chat improved + jade > html

--- chat/serv.js
+++ chat/serv.js
@@ -1,29 +1,27 @@
 var http = require('http');
 var fs = require('fs');
+var jade = require('jade');
 var express = require('express');
 var session = require('cookie-session');
 
 var app = express();
 
 var server = http.createServer(function(req, res) {
-	fs.readFile('./index.html', 'utf-8', function(error, content) {
-		res.writeHead(200, {"Content-Type": "text/html"});
-		res.end(content);
-	});
+	var html = jade.renderFile('index.jade');
+	res.writeHead(200, {"Content-Type": "text/html"});
+	res.end(html);
 });
 
 var io = require('socket.io').listen(server);
 
 io.sockets.on('connection', function(socket){
-	socket.emit('get_user');
-	socket.on('test', function(socket){
-		console.log('test passed');
+	socket.on('nouvo', function(socket){
+		console.log('nouvo catch ki arriv ! son blaz c : ' + socket.user + ' !');
 	});
-	socket.on('arrivee', function(username){
+	socket.on('msg', function(msg){
 		console.log('RECU');
-		console.log(username.user);
+		console.log(msg.user + '  dit : ' + msg.text);
 	});
 });
-
 
 server.listen(1337);
