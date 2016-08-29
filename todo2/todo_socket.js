fb7923a7c018c20fb60cb3adccfba439d143195a
Thibault FRANCOIS <thifranc@e3r10p2.42.fr>
2016-08-30 00:26:20 +0200

    todo socket handled, see at connection

--- todo2/todo_socket.js
+++ todo2/todo_socket.js
@@ -2,7 +2,7 @@ var bodyparser = require('body-parser');
 var http = require('http');
 var jade = require('jade');
 
-var todo = {bonjour : 'bonjour'};
+var todo = [];
 
 var server = http.createServer(function(req, res){
 	var html = jade.renderFile('./views/index.jade');
@@ -13,8 +13,13 @@ var server = http.createServer(function(req, res){
 var io = require('socket.io').listen(server);
 
 io.sockets.on('connection', function(socket){
-	console.log(socket);
-	console.log(todo);
+	console.log('connected!');
+	socket.on('new_todo', function(text){
+		todo.push(text.text);
+		console.log(todo);
+		socket.broadcast.emit('new_todo', {'todo': todo});
+		});
 	});
 
+console.log('listening on 1337');
 server.listen(1337);
